45ece10f59805939d5b44254de18e4c9fa943e59
madinashukurova <mshukurova85@gmail.com>
2022-04-06 19:39:32 -0400

    Created the loop in fun

---
+++ src/task_cases/Problem.java
@@ -0,0 +1,10 @@
+package task_cases;
+
+public class Problem {
+
+    public static void main(String[] args) {
+        for (int i = 0; i < 5; i++){
+            System.out.println(i);
+        }
+    }
+}
